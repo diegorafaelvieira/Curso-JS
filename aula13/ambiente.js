4fae6fdd4110af01abe4bf3bc375ac193c54a24c
diegorafaelvieira <diegorafaelvieira@bol.com.br>
2019-09-12 22:25:36 -0300

    Add Aula 13 e Aula 14.

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+//EXECUTA E DEPOIS TESTA O BLOCO
+console.log(`DO WHILE`)
+var c = 1
+do{
+    console.log(`Tudo bem? ${c}`)
+    c++   
+} while (c <= 6)
+
+//TESTA E DEPOIS EXECUTA O BLOCO
+console.log(`WHILE`)
+var c = 1
+while (c <= 6) {
+    console.log(`Tudo bem? ${c}`)
+    c++  // c = c + 1 
+}
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,25 @@
+console.log('VAI COMEÇAR...')
+
+
+console.log('WHILE')
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+
+
+console.log('FOR')
+for(var c = 1; c <= 5;c++){
+    console.log(c)
+}
+
+console.log('FIM!')
+
+/*
+DEBUGAR
+DEBUG->Start Debugging
+
+Watch do c
+pressionar f10 até o fim 
+*/
