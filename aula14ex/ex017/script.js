6c9af219d1d59147fbe99ecbfe8c2ac89f22e213
diegorafaelvieira <diegorafaelvieira@bol.com.br>
2019-09-14 12:04:05 -0300

    Add Exercício 17 Curso JS.

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function multiplicar() {
+    let val = document.getElementById('txtv')
+    let tab = document.getElementById('seltab')
+
+    if(val.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let v = Number(val.value)
+        tab.innerHTML = ''
+        for(let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${v} x ${i} = ${v*i} `
+            item.value = `tab${i}` //Usado no PHP
+            tab.appendChild(item)
+        }
+    }
+}
